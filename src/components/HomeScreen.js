ea79bc896de446d39e40541405045d1cb513cd4e
NMShihab <nmshihabislam@gmail.com>
2021-02-09 12:52:03 +0600

    row component

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,22 +1,30 @@
-import React from 'react'
-import '../css/HomeScreen.css'
-import Banner from './Banner'
-import NavBar from './NavBar'
+import React from "react";
+import "../css/HomeScreen.css";
+import requests from "../Requests";
+import Banner from "./Banner";
+import NavBar from "./NavBar";
+import Row from "./Row";
 
 function HomeScreen() {
-    
-    return (
-        <div className = "homeScreen">
-            {/* Navbar */}
-            <NavBar />
-            
-            {/* Banner */}
-            <Banner />
-
-            {/* Row */}
-            
-        </div>
-    )
+  return (
+    <div className="homeScreen">
+      {/* Navbar */}
+      <NavBar />
+      {/* Banner */}
+      <Banner />
+      {/* Row */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow="true"
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romantic Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
