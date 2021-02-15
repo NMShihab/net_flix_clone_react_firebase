9e5d4a896805a0a3e7ab7e3e25183ab3de2e307b
NMShihab <nmshihabislam@gmail.com>
2021-02-15 15:01:59 +0600

    login page styling

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -6,9 +6,30 @@ function Login() {
     <div className="login">
       <div className="login__background">
         <img
-          src="https://i.pinimg.com/originals/41/6a/7f/416a7fd4720a3c0dbd38c17f3ac8e0c1.jpg"
+          className="login__logo"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
+
+        <button className="login__button">Sign in</button>
+        <div className="login__gradient" />
+
+        <div className="login__body">
+          <>
+            <h1>Unlimited films, TV Programmes and more.</h1>
+            <h2>Watch anywhere . Cancel anytime</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </h3>
+            <div className="login__input">
+              <form action="">
+                <input type="email" placeholder="Email Address" />
+                <button className="login__getStarted">Get Started</button>
+              </form>
+            </div>
+          </>
+        </div>
       </div>
     </div>
   );
