63a922dce778d9f45bcb2d1048cb43a9419c35d7
NMShihab <nmshihabislam@gmail.com>
2021-02-17 14:59:15 +0600

    style profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -20,6 +20,7 @@ function Profile() {
           <div className="profile__details">
             <h2>{user.email}</h2>
             <div className="profile__plans">
+              <h3>Plans</h3>
               <button
                 onClick={() => auth.signOut()}
                 className="profile__signOut"
